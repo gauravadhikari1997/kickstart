a418aa7513290aa567aea51519409040fea5c600
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-04-14 00:17:11 +0530

    package json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const routes = require("./routes");
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
+  createServer(handler).listen(process.env.PORT || 3000, err => {
     if (err) throw err;
     console.log("Ready on localhost:3000");
   });
