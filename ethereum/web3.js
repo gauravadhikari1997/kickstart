3272ddf05ba79e76e43530d0e94fc1f2cccdb177
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-04-14 15:24:41 +0530

    Next.js app on Heroku

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -4,6 +4,7 @@ let web3;
 
 if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // if we are in browser and Metamask is installed
+  window.ethereum.enable();
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // we are on server or user is not running Metamask
