153ba21dbb3978a5b02c10a49500c8f630261b52
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-04-15 02:16:50 +0530

    ethereum crowdcoin app

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,7 +3,7 @@ import Web3 from "web3";
 let web3;
 
 if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
-  // if we are in browser and Metamask is installed
+  // if we are in browser and Metamask is already installed
   window.ethereum.enable();
   web3 = new Web3(window.web3.currentProvider);
 } else {
