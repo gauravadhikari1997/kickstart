53a367fd76b2a4ef63d3c5d427ac0ad369da8249
unknown <gaurav.15bme2023@abes.ac.in>
2020-04-28 15:30:54 +0530

    routing issue

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const routes = require("next-routes")();
 
 routes.add("/campaigns/new", "/campaigns/new");
 routes.add("/campaigns/:address", "/campaigns/show");
-routes.add("/campaigns/:address/requests", "/requests/index");
-routes.add("/campaigns/:address/requests/new", "/requests/new");
+routes.add("/campaigns/:address/requests", "/campaigns/requests/index");
+routes.add("/campaigns/:address/requests/new", "/campaigns/requests/new");
 
 module.exports = routes;
